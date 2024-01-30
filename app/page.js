6dc75ebcaa07f0378d8f9614896127888400986f
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-01-30 18:53:07 +0300

    minor layout adjustments

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import app from "./firebase"
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-12 bg-gray-900">
+		<main className="flex flex-col items-center justify-between p-12 ">
 			<p className="text-white">
 				Chat Application - In Order to Use It, You Need To Login
 			</p>
